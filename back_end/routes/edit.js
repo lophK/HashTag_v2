4a4f4a7a23d8c1e2a905ba0e42802a39a4c15c82
lophKIV <konoiza.09@gmail.com>
2021-01-22 01:57:08 +0700

    edit st

--- back_end/routes/edit.js
+++ back_end/routes/edit.js
@@ -4,23 +4,25 @@ var db = require('../condb');
 
 var express = require('express');
 var router = express.Router();
+var Password = require("node-php-password");
 
 
-router.post('/edit_ac', function (req, res, next) {
+router.post('/edit_ac', function (req, res) {
 
         var id= req.body.id;
         var fname= req.body.first_name;
         var lname= req.body.last_name;
-        var user= req.body.username;
+        var email= req.body.email;
         var pass= req.body.password;
         var tel= req.body.tel_phone;
         var add= req.body.address;
         var bir= req.body.birthday;
+        var img= req.body.user_img;
 
     //UPDATE  account_user  SET first_name="t", last_name="t",username="t", password="161042", tel_phone="t", address="t", birthday="2000-10-16" where id="1"
     //'UPDATE  account_user  SET first_name='+fname+',last_name='+lname+','+'username='+user+',password='+pass+',tel_phone='+tel+',address='+add+',birthday='+bir+' where id='+id+''
     console.log(fname + "   " + lname);
-    db.query('UPDATE  account_user  SET first_name="'+fname+'", last_name="'+lname+'",username="'+user+'", password="'+pass+'", tel_phone="'+tel+'", address="'+add+'", birthday="'+bir+'"'+"where id="+id+'', function (error, results, fields) {
+    db.query('UPDATE  account_user  SET first_name= "'+fname+'", last_name= "'+lname+'",email= "'+email+'", password= "'+pass+'", tel_phone= "'+tel+'", address= "'+add+'", birthday= "'+bir+'", user_img= "'+img+'" '+"where id="+id+'', function (error, results, fields) {
         if(error) throw error
         if(error) {
             //console.log(error);
@@ -31,9 +33,27 @@ router.post('/edit_ac', function (req, res, next) {
             res.status(200).json({message: "Done"});       
           }
     });
+ 
+});
+router.post('/edit_ac2', function (req, res,next) {
+  const { email, password: plainTextPassword, first_name, last_name, birthday, tel_phone, address} = req.body;
+  const password = Password.hash(plainTextPassword);
+  //res.send('s');
+  let sql = 'UPDATE  account_user SET first_name=?,last_name=? ,email=?, password=?, tel_phone=?, address=?, birthday=?, user_img=?'+'where id = ?';
+        
+            sql = db.format(sql, [
+               first_name, last_name,email,password,tel_phone,  address  ,  birthday ,user_img , id
+            ]);
+            db.query(sql, (error, results, fields) => {
+                if (error) throw error;
+                if (results.affectedRows > 0) {
+                    res.status(200).send(true);
+                } else {
+                    res.status(200).send(false);
+                }
+            });
+
+});
 
 
-
-
-})
 module.exports = router;
