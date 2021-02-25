f8cf4c03b23f8b214a8c8c27b0ef56b09bdc2105
lophKIV <konoiza.09@gmail.com>
2021-02-25 21:05:44 +0700

    ปปป

--- back_end/routes/select.js
+++ back_end/routes/select.js
@@ -12,12 +12,13 @@ router.post('/select_post', function(req, res, next) {
    
     const { email} = req.body;
     var GRAB_post = `SELECT
-	db_loph.post.post_id, 
-	db_loph.IMG_file.*
+	db_loph.IMG_file.*, 
+	db_loph.post.*
 FROM
 	db_loph.IMG_file
 	INNER JOIN
-	db_loph.post	
+	db_loph.post
+	
 WHERE
     db_loph.post.email_ac = '`+req.body.email_ac+`'`;
     //ON 
