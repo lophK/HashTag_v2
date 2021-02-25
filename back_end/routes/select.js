79b8841cb1720f4e43ed9cf5ec889711358799d8
lophKIV <konoiza.09@gmail.com>
2021-02-25 21:16:45 +0700

    ko

--- back_end/routes/select.js
+++ back_end/routes/select.js
@@ -8,7 +8,7 @@ var  json  = require('body-parser');
 
 
 
-router.post('/select_post', function(req, res, next) {
+router.post('/select_imgpost', function(req, res, next) {
    
     const { email} = req.body;
     var GRAB_post = `SELECT
@@ -32,8 +32,32 @@ WHERE
         var user = result
         console.log(user);
        
-          return res.json({ email: 'sss' });
+          return res.json({ email: req.body.email });
       }
       })
   });
+  router.post('/select_post_email', function(req, res, next) {
+   
+    const { email} = req.body;
+    var GRAB_post = `  SELECT
+	db_loph.post.*
+FROM
+	db_loph.post
+WHERE
+	db_loph.post.email_ac = '`+req.body.email+`'`;
+    //ON 
+        // db_loph.IMG_file.post_id = db_loph.post.post_id
+    db.query(GRAB_post, req.body.email, (err, result) => {
+      if (err) {
+        res.json({message:"Error"})
+      } 
+      else if (result) {
+        var user = result
+        console.log(user);
+       
+          return res.json({ email: req.body.email });
+      }
+      })
+  });
+
   module.exports = router;
