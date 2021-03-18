622e1c6c8f39e56543fee2ae8ac0a3ea9dced4dc
lophKIV <konoiza.09@gmail.com>
2021-03-19 04:36:46 +0700

    111

--- back_end/routes/delete.js
+++ back_end/routes/delete.js
@@ -27,6 +27,29 @@ router.post('/del_ac', function (req, res, next) {
 
 
 })
+
+router.post('/del_post', function (req, res, next) {
+
+  var post_id= req.body.id;
+ // where id="+id+''
+console.log(id);
+db.query('DELETE FROM post WHERE post_id ='+post_id+'', function (error, results, fields) {
+  if(error) throw error
+  if(error) {
+      //console.log(error);
+        res.status(500).json({message: "Invalid Request"});
+    } else {
+      //let inserted_id = results.id; // undefined
+      //console.log(inserted_id);   
+      res.status(200).json({message: "Done"});       
+    }
+});
+
+
+
+
+})
+
 router.post('/comment-del', async function  (req, res, next) {
   //const { email, password: plainTextPassword, first_name, last_name, birthday, tel_phone, address, user_img} = req.body;
  
