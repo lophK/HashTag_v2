1fbc37d74dfa5a153f226ccf1fd82954685500c7
narathip15031999 <naratihporiginal@gmail.com>
2021-03-25 10:39:29 +0700

    oil

--- HashTagV2/src/app/home/home.component.ts
+++ HashTagV2/src/app/home/home.component.ts
@@ -71,6 +71,7 @@ export class HomeComponent implements OnInit {
   editpostid : any
   editimage  = false
   detail1 = ''
+  token : any = localStorage.getItem('token')
   showDialog(id: any) {
     this.idpost = id;
     let json = { post_id: this.idpost }
@@ -425,9 +426,8 @@ export class HomeComponent implements OnInit {
   }
 
   async editpost() {
-    let JWT =localStorage.getItem('token');
-    console.log(JWT);
-    let json = { tag_id: this.tag_id1, post_detail: this.detail1, post_status: this.Poststatus1, post_id: this.editpostid, email_ac: this.email,token:JWT}
+    // let JWT : any =localStorage.getItem('token');
+    let json = { tag_id: this.tag_id1, post_detail: this.detail1, post_status: this.Poststatus1, post_id: this.editpostid, email_ac: this.email,token: this.token}
     console.log(json)
     await this.http.post('http://localhost:3120/edit/edit_post', (json)).subscribe(response => {
       location.reload();
